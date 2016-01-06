7407d7dee57feaf21792a789aca42eaa3e5acfc9
charlesmarvin <marvin.charles@gmail.com>
2016-01-05 21:07:08 -0500

    Add handler for creating and updating instructors

--- src/main/java/com/dsmc/api/instructors/InstructorResource.java
+++ src/main/java/com/dsmc/api/instructors/InstructorResource.java
@@ -3,18 +3,25 @@ package com.dsmc.api.instructors;
 import com.dsmc.api.auth.AuthUserRequestManager;
 import com.dsmc.api.core.transformers.JsonTransformer;
 import com.dsmc.api.core.transformers.SerializationProvider;
+import com.dsmc.data.tables.pojos.Instructor;
+
+import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
+import static spark.Spark.put;
 
 public class InstructorResource {
     private final String context;
     private final InstructorService service;
     private final SerializationProvider serializationProvider;
+    private final JsonTransformer transformer;
 
     public InstructorResource(String context, InstructorService service, SerializationProvider serializationProvider) {
         this.context = context;
         this.service = service;
         this.serializationProvider = serializationProvider;
+        transformer = new JsonTransformer(serializationProvider);
         configureRoutes();
     }
 
@@ -22,14 +29,33 @@ public class InstructorResource {
         get(context + "instructors", (request, response) -> {
                     Integer companyId = AuthUserRequestManager.getCompanyId(request);
                     return service.getInstructors(companyId);
-                }, new JsonTransformer(serializationProvider)
+                }, transformer
         );
 
         get(context + "instructors/:id", (request, response) -> {
                     Integer companyId = AuthUserRequestManager.getCompanyId(request);
                     Integer instructorId = Integer.parseInt(request.params("id"));
                     return service.getInstructorById(companyId, instructorId);
-                }, new JsonTransformer(serializationProvider)
+                }, transformer
+        );
+
+        post(context + "students", (request, response) -> {
+                    Integer companyId = AuthUserRequestManager.getCompanyId(request);
+                    Instructor instructor = serializationProvider.get().fromJson(request.body(), Instructor.class);
+                    service.create(companyId, instructor);
+                    response.status(201);
+                    return "OK";
+                }, transformer
+        );
+
+        put(context + "instructors/:id", (request, response) -> {
+                    Integer companyId = AuthUserRequestManager.getCompanyId(request);
+                    Integer instructorId = Integer.parseInt(request.params("id"));
+                    Map<String, ?> updates = serializationProvider.get().fromJson(request.body(), Map.class);
+                    service.update(companyId, instructorId, updates);
+                    response.status(201);
+                    return "OK";
+                }, transformer
         );
     }
 
--- src/main/java/com/dsmc/api/instructors/InstructorService.java
+++ src/main/java/com/dsmc/api/instructors/InstructorService.java
@@ -1,12 +1,20 @@
 package com.dsmc.api.instructors;
 
 import com.dsmc.data.tables.pojos.Instructor;
+import com.dsmc.data.tables.records.InstructorRecord;
+import com.google.common.base.CaseFormat;
 import org.jooq.DSLContext;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class InstructorService {
     private final DSLContext context;
+    private Collection<String> nonUpdatableFields = Arrays.asList("id", "companyId", "createdOn");
+
 
     public InstructorService(DSLContext context) {
         this.context = context;
@@ -28,5 +36,28 @@ public class InstructorService {
                 .where(i.COMPANY_ID.equal(companyId).and(i.ID.equal(instructorId)))
                 .fetchOne()
                 .into(Instructor.class);
+    }
+
+    public void create(Integer companyId, Instructor instructor) {
+        InstructorRecord instructorRecord = context.newRecord(com.dsmc.data.tables.Instructor.INSTRUCTOR, instructor);
+        instructorRecord.setCompanyId(companyId);
+        instructorRecord.setActive("Y");
+        instructorRecord.store();
+    }
+
+    public void update(Integer companyId, Integer instructorId, Map<String, ?> updates) {
+        com.dsmc.data.tables.Instructor s = com.dsmc.data.tables.Instructor.INSTRUCTOR.as("s");
+        InstructorRecord instructorRecord = context.selectFrom(s)
+                .where(s.COMPANY_ID.equal(companyId).and(s.ID.equal(instructorId)))
+                .fetchOne();
+        if (instructorRecord == null) {
+            throw new RuntimeException("Attempt to update unknown record");
+        }
+        Map<String, Object> recordUpdates = updates.entrySet().stream()
+                .filter(entry -> entry.getValue() != null)
+                .filter(entry -> !nonUpdatableFields.contains(entry.getKey()))
+                .collect(Collectors.toMap(entry -> CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, entry.getKey()), Map.Entry::getValue));
+        instructorRecord.fromMap(recordUpdates);
+        instructorRecord.store();
     }
 }
